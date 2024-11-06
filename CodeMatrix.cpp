a62b0834a1d8d4ce060940de16d06c16b505c8e8
Ryan Clark <mail.w3geek@gmail.com>
2024-11-06 04:06:25 -0500

    Implement the stubbed method update to generate a random hex value and assign it to the table.
    TODO: Move Random Hex Value Assignment to constructor.

--- CodeMatrix.cpp
+++ CodeMatrix.cpp
@@ -24,6 +24,12 @@ void CodeMatrix::Render(void) {
 }
 
 void CodeMatrix::Update(void) {
-
+  // Generates a Random Hex Value and adds it the corresponding table slot.
+  for (int index = 0; index < 6; index++) {
+    for (int hex = 0; hex < 6; hex++) {
+      unsigned char randomHexValue = rand() % 256;
+      this->table[index][hex] = randomHexValue;
+    }
+  }
 }
 
